6c904f47009a34ba7fc0296031c9961b313c1767
koganemarugithub <koganemaru.sunao@gmail.com>
2015-10-17 06:48:30 +0000

    destroy_function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
      # Messageを全て取得する。
@@ -7,29 +7,34 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
   
+  def create
+  @message = Message.new(message_params)
+    if @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+    # メッセージが保存できなかった時
+    @messages = Message.all
+    flash.now[:alert] = "メッセージの保存に失敗しました。"
+    render 'index'
+    end
+  end
+  
   def edit
   end  
 
   def update
-    if @message/update(message_params)
+    if @message.update(message_params)
       # 保存に成功した場合はトップページへリダイレクト
       redirect_to root_path , notice: 'メッセージを編集しました'
     else
-      #　保存に失敗した場合は編集画面へ戻す
+      # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
     end
   end
   
-  def create
-    @message = Message.new(message_params)
-    if @message.save
-      redirect_to root_path , notice: 'メッセージを保存しました'
-    else
-      # メッセージが保存できなかった時
-      @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
-      render 'index'
-    end
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
   private
