329f69ba5a0ec1132dc03c4a37a3525e21f04ca1
koganemarugithub <koganemaru.sunao@gmail.com>
2015-10-17 12:21:30 +0000

    config.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module MessageBoard
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.consider_all_requests_local = true
   end
 end
